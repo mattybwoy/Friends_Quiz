c059dc0f5eb560bc2e9222fc510d56090dc48ba0
Matthew Lock <matthew_lock20@hotmail.com>
2022-06-23 08:53:50 +0100

    Initial Commit

---
+++ Friends Quiz/Friends_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friends_QuizApp.swift
+//  Friends Quiz
+//
+//  Created by Matthew Lock on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Friends_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
